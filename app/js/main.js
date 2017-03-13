e24a7184b5842d565040e031766b4fd25f1f9832
aline Binet-Teissonnière <alinebinet@MacBook-Pro-de-aline.local>
2017-03-13 10:21:21 +0100

    intégration du module de connexion v.1.0

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,33 @@
+jQuery.noConflict();
+jQuery(function($) {
 
+	$(document).ready(function() {
+
+
+		$("#login-form").validate();
+		$( "#login-form" ).validate({
+		  rules: {
+		    confirm_1: {
+		      require_from_group: [1, ".confirmCode"]
+		    },
+		    confirm_2: {
+		      require_from_group: [1, ".confirmCode"]
+		    },
+		    confirm_3: {
+		      require_from_group: [1, ".confirmCode"]
+		    },
+		    confirm_4: {
+		      require_from_group: [1, ".confirmCode"]
+		    },
+		    confirm_5: {
+		      require_from_group: [1, ".confirmCode"]
+		    },
+		    confirm_6: {
+		      require_from_group: [1, ".confirmCode"]
+		    },
+		  }
+		});
+
+	}); //End document ready
+
+}); // End jQuery
